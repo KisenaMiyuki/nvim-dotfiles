89bab00a75bbf39541c9bdc91e5cdb9aba55fd4b
Miyuki@Silvia <1336286009@qq.com>
2023-07-25 19:11:07 +0800

    更新plugins列表

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,10 +1,29 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
+    {
+    	"folke/tokyonight.nvim",
+    	lazy = false,
+    	priority = 1000,
+    	dependencies = {
+    		-- lualine & dependencies
+    		"nvim-lualine/lualine.nvim",
+    		"nvim-tree/nvim-web-devicons",
+    		-- barbecue & denpendencies
+    		"utilyre/barbecue.nvim",
+    		"SmiteshP/nvim-navic",
+    	},
+    	config = function()
+    		-- enable tokyonight theme on load
             vim.cmd[[colorscheme tokyonight-night]]
-		end
-  	}
+            -- set lualine theme to tokyonight
+            require('lualine').setup({
+            	options = {
+            		theme = "tokyonight"
+            	}
+            })
+            -- set barbeque theme to tokyonight
+            require('barbecue').setup({
+            	theme = "tokyonight"
+            })
+    	end
+	}
 }
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        config = true,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+    	    require('indent_blankline').setup({
+    		    show_first_indent_level = false
+    	    })
+    	end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        end
+    },
+    {
+        "RRethy/vim-illuminate",
+        config = function()
+            require('illuminate').configure()
+        end
+    }
+}
